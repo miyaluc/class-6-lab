0af633259760d4dc977e5a3e413cf17b215e7cc2
Miya Lucas <srlucas3@gmail.com>
2017-06-08 21:04:35 -0700

    added hours key to the object and fixed my customers per hour method

--- app.js
+++ app.js
@@ -4,19 +4,27 @@ var firstAndPike = {
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
   averageCookiesPerCustomer: 6.3,
-  averageCustomersPerHour: function() {
+  averageCustomersPerHour: function(minHourlyCustomers, maxHourlyCustomers) {
     return Math.floor (Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;},
+  hours: ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM']
 };
 
+console.log ('Store data: ' + firstAndPike);
+
 firstAndPike.averageCustomersPerHour ();
+
+var cookiesPerHour = firstAndPike.averageCookiesPerCustomer * firstAndPike.averageCustomersPerHour;
 
 var seatacAirport = {
   minHourlyCustomers: 3,
   maxHourlyCustomers: 24,
   averageCookiesPerCustomer: 1.2,
-  averageCustomersPerHour: function() {
+  averageCustomersPerHour: function(minHourlyCustomers, maxHourlyCustomers) {
     return Math.floor (Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;},
+  hours: ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM']
 };
+
+console.log ('Store data: ' + seatacAirport);
 
 seatacAirport.averageCustomersPerHour();
 
@@ -24,9 +32,12 @@ var seattleCenter = {
   minHourlyCustomers: 11,
   maxHourlyCustomers: 38,
   averageCookiesPerCustomer: 3.7,
-  averageCustomersPerHour: function() {
+  averageCustomersPerHour: function(minHourlyCustomers, maxHourlyCustomers) {
     return Math.floor (Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;},
+  hours: ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM']
 };
+
+console.log ('Store data: ' + seattleCenter);
 
 seattleCenter.averageCustomersPerHour();
 
@@ -34,9 +45,12 @@ var capitolHill = {
   minHourlyCustomers: 20,
   maxHourlyCustomers: 38,
   averageCookiesPerCustomer: 2.3,
-  averageCustomersPerHour: function() {
+  averageCustomersPerHour: function(minHourlyCustomers, maxHourlyCustomers) {
     return Math.floor (Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;},
+  hours: ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM']
 };
+
+console.log ('Store data: ' + capitolHill);
 
 capitolHill.averageCustomersPerHour();
 
@@ -44,8 +58,11 @@ var alki = {
   minHourlyCustomers: 2,
   maxHourlyCustomers: 16,
   averageCookiesPerCustomer: 4.3,
-  averageCustomersPerHour: function() {
+  averageCustomersPerHour: function(minHourlyCustomers, maxHourlyCustomers) {
     return Math.floor (Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers;},
+  hours: ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM']
 };
+
+console.log ('Store data: ' + alki);
 
 alki.averageCustomersPerHour();
