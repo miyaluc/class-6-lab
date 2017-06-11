110e093c2894f2cc2ddca71911f67a10a12662d5
Miya Lucas <srlucas3@gmail.com>
2017-06-10 17:27:14 -0700

    added some lab 7 work into lab 6 and commented it out

--- app.js
+++ app.js
@@ -214,3 +214,20 @@ alki.cookiesPerHourMethod();
 alki.displayCookieList();
 
 console.log (alki);
+
+//started working on lab 7 below
+
+// function Store(storeLocation, minHourlyCustomers,maxHourlyCustomers, averageCookiesPerCustomer) {
+//   this.storeLocation = storeLocation;
+//   this.minHourlyCustomers = minHourlyCustomers;
+//   this.maxHourlyCustomers = maxHourlyCustomers;
+//   this.averageCookiesPerCustomer = averageCookiesPerCustomer;
+// }
+//
+// var firstAndPike = new Store('1st Ave and Pike St', 23, 65, 6.3);
+// var seatacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
+// var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+// var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+// var alki = new Store('Alki', 2, 16, 4.3);
+//
+// console.log(Store);
